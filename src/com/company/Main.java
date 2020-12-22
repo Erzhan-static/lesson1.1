07a43e6ee2e7f81c1bd73f90db02b7160b12ab3f
Erzhan <bakalbaev.erzhan@gmail.com>
2020-12-22 23:41:23 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println((1+2)-4*3/2);
+        System.out.println(7+1);
+        System.out.println("result"+(7+88));
+
+        int ageOfnumberStudent;//объявление пременной
+        ageOfnumberStudent=7;//объявление пременной
+
+        int ageOfperson=22;
+        System.out.println(ageOfperson);
+        System.out.println(ageOfperson+"возраст человека");
+        System.out.println(ageOfperson+7);
+        ageOfperson=30;//переопределение значения пременной
+        System.out.println(ageOfperson);
+
+        boolean isItNight=true;
+        System.out.println(isItNight);
+
+        double digit=7.89;
+
+        final int NUM=8;//неизменимая переменная (константа)
+
+
+
+        }
+
+
+
+    }
